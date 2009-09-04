8ffa4a56a8dcc479f4c1b934014d2fbb74f7b21e
simpsonjulian <simpsonjulian@gmail.com>
2009-09-04 15:43:18 +0100

    add the etc dir

--- manifests/defines/wordpress.pp
+++ manifests/defines/wordpress.pp
@@ -1,10 +1,15 @@
 define wordpress::blog ($fq_host, $password) {
   include wordpress::installation
   file {
+    "wordpress etc dir":
+      path => "/etc/wordpress",
+      ensure => directory;
+      
     "wordpress config":
       path => "/etc/wordpress/config-${fq_host}.php",
       ensure => file,
-      content => template("wordpress/config-fqdn.php.erb");
+      content => template("wordpress/config-fqdn.php.erb"),
+      require => File["wordpress etc dir"];
   }
   
 }
