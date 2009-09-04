3bb2be851056c69cf149bcaa8b741dc0b9a6c396
simpsonjulian <simpsonjulian@gmail.com>
2009-09-04 13:52:00 +0100

    get the names in synch

--- manifests/defines/wordpress.pp
+++ manifests/defines/wordpress.pp
@@ -4,7 +4,7 @@ define wordpress::blog ($fq_host, $password) {
     "wordpress config":
       path => "/etc/wordpress/config-${fq_host}.php",
       ensure => file,
-      content => template("wordpress/config-fqdn.pp");
+      content => template("wordpress/config-fqdn.php");
   }
   
 }
