8be92a64939007e60ad122e3434ee1754916e36a
simpsonjulian <simpsonjulian@gmail.com>
2009-12-13 14:38:14 +0000

    require a class

--- manifests/defines/wordpress.pp
+++ manifests/defines/wordpress.pp
@@ -1,13 +1,12 @@
 define wordpress::blog ($name, $user, $fq_host, $password) {
   
   file {
-      
     "wordpress config ${fq_host}":
       path => "/etc/wordpress/config-${fq_host}.php",
       ensure => file,
       content => template("wordpress/config-fqdn.php.erb"),
-      require => File["wordpress etc dir"];
-     
+      require => File["wordpress etc dir"],
+      require => Class["wordpress::installation"];
   }
   
 }
