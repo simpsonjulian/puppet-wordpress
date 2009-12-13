04085a3c3210fd88903eddceb124f32c1b83013b
simpsonjulian <simpsonjulian@gmail.com>
2009-12-13 13:04:53 +0000

    enable supercache

--- files/data/wordpress/wp-config.php
+++ files/data/wordpress/wp-config.php
@@ -6,6 +6,6 @@ Please do NOT edit and read about how the configuration works in the README.Debi
 require_once('/etc/wordpress/config-'.strtolower($_SERVER['HTTP_HOST']).'.php');
 
 define('ABSPATH', '/data/wordpress/');
-
+define('WP_CACHE', true);
 require_once(ABSPATH.'wp-settings.php');
 ?>
